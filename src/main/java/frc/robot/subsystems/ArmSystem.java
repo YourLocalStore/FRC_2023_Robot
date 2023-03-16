9521b77eec5734b28fbca8b26b8da2e5592e5e41
LocalStore <84473197+YourLocalStore@users.noreply.github.com>
2023-03-16 15:26:14 -0400

    amazing arm code by localstore

--- src/main/java/frc/robot/subsystems/ArmSystem.java
+++ src/main/java/frc/robot/subsystems/ArmSystem.java
@@ -7,13 +7,26 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class ArmSystem extends SubsystemBase{
 
-    private final WPI_VictorSPX m_leftMotor;
+    private final CANSparkMax m_topMotor1; // Elbow Motor
+    private final CANSparkMax m_bottomMotor1; // Arm Motor
+    private final Encoder shoulder_encoder, elbow_encoder; // Encoders
+
+    private int shoulder_position = 0 // 
+    private int elbow_position = 0 // 
 
     public ArmSystem(){
-        m_leftMotor = new WPI_VictorSPX(6);
+        m_topMotor1 = new CANSparkMax (6); 
+        m_bottomMotor1 = new Spark(6)
+        m_motor = new MotorControllerGroup(m_topMotor1, m_bottomMotor1);
+
     }
 
-    void OperateArm(double speed){
-        m_leftMotor.set(speed);
+    // Arm Pos
+    public void getArmPosition;
+
+    // Arm Speed
+    public void OperateArm(double speed){
+        m_topMotor1.set(speed);
+        m_bottomMotor1.set(speed);
     }
 }
